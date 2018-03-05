87ee65d39e8a0ec23a4f370fb85051040479315e
David Pazdera <david.pazdera@hotmail.com>
2018-03-05 20:43:32 +0100

    Added PSH snippets for deployments and dashboard export.

---
+++ Scripts/Episode1/deployment-cmdlets.ps1
@@ -0,0 +1,34 @@
+## Code snippets for working with ARM deployments
+
+# Get all deployments for a resource group
+Get-AzureRmResourceGroupDeployment -ResourceGroupName "k8sjenspin-dev-rg"
+# Output structure (objects): 
+# DeploymentName, ResourceGroupName, ProvisioningState, Timestamp, Mode, 
+# TemplateLink, Parameters (Name, Type, Value), Outputs (Name, Type, Value), DeploymentDebugLogLevel
+
+
+# Get a deployment by name
+Get-AzureRmResourceGroupDeployment -ResourceGroupName "k8sjenspin-dev-rg" -Name "Microsoft.Template"
+# Outputs: Microsoft.Azure.Commands.ResourceManagement.Models.PSResourceGroupDeployment
+
+# Validate a resource group deployment:
+# The Test-AzureRmResourceGroupDeployment cmdlet determines whether an Azure resource group deployment
+# template and its parameter values are valid.
+Test-AzureRmResourceGroupDeployment -ResourceGroupName "k8sjenspin-dev-rg" -TemplateFile "azuredeploy.json"
+
+$ParamsHashTable = @{}
+Test-AzureRmResourceGroupDeployment -ResourceGroupName "k8sjenspin-dev-rg" -TemplateFile "azuredeploy.json" -TemplateParameterObject $ParamsHashTable
+
+Test-AzureRmResourceGroupDeployment -ResourceGroupName "k8sjenspin-dev-rg" -TemplateUri "https://anyURL" -TemplateParameterObject $ParamsHashTable
+
+# Export a deployment to a template
+Export-AzureRmResourceGroup -ResourceGroupName "k8sjenspin-dev-rg" -Path c:\temp -IncludeParameterDefaultValue -IncludeComments
+# Note: There might be some cases where this cmdlet fails to generate some parts of the template. 
+# Warning messages will inform you of the resources that failed. 
+# The template will still be generated for the parts that were successful.
+
+# Get deployment operations for a particular deployment
+Get-AzureRmResourceGroupDeploymentOperation -DeploymentName 'Microsoft.Template' -ResourceGroupName 'k8sjenspin-dev-rg'
+# lists all the operations that were part of a deployment to help you identify and give more information about 
+# the exact operations that failed for a particular deployment. It can also show the response and the request 
+# content for each deployment operation.
